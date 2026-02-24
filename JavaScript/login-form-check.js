96a7a7fb1ed1ffcb5ab0b079065c65d5ce33e644
Esteban Mercado <esteban.mercadom99@gmail.com>
2026-02-24 17:04:02 -0600

    Se agrega funcionalidad de creacion de objetos Json en SignUp

---
+++ JavaScript/login-form-check.js
@@ -0,0 +1,55 @@
+const form = document.getElementById('loginForm')
+
+document.addEventListener('DOMContentLoaded', function () {
+    // seedUsers();
+})
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+
+    //const objectData = Object.fromEntries([...new FormData(form)])
+    const data = new FormData(form)
+    const arrayData = [...data]
+    const objectData = Object.fromEntries(arrayData)
+
+    const retrievedData = localStorage.getItem('users')
+    const retrievedObject = JSON.parse(retrievedData)
+
+    const user = retrievedObject.find(user => user.email === objectData.email)
+
+    if (!user) {
+        const feedbackElement = document.getElementById('user-not-found')
+        const wrapper = document.createElement('div')
+        wrapper.innerHTML = [
+            `<div class="alert alert-danger alert-dismissible mt-2" role="alert">`,
+            `   <div>Usuario o contraseña incorrectos, intenta de nuevo</div>`,
+            '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+            '</div>'
+        ].join('')
+
+        feedbackElement.append(wrapper)
+    }
+
+    else{
+        if(user.password === objectData.password){
+            console.log("sí es")
+            window.location.href = "index.html"
+        }
+    }
+
+})
+
+async function seedUsers() {
+    if (!localStorage.getItem('users')) {
+        try {
+            const response = await fetch('data/users.json');
+            if (response.ok) {
+                const mockUsers = await response.json();
+                localStorage.setItem('users', JSON.stringify(mockUsers));
+                console.log('LocalStorage seeded with mock users.');
+            }
+        } catch (error) {
+            console.error('Error seeding mock users:', error);
+        }
+    }
+}
--- JavaScript/signup-form-check.js
+++ JavaScript/signup-form-check.js
@@ -31,16 +31,15 @@ function checkPhoneNumber() {
         }
         // 4. Update the input field
         this.value = formattedValue
-        if(this.value.length === 12){
+        if (this.value.length === 12) {
             this.classList.remove('is-invalid');
             this.classList.add('is-valid');
-            return true;
         }
-        else if(this.value === ''){
+        else if (this.value === '') {
             this.classList.remove('is-invalid');
             this.classList.remove('is-valid');
         }
-        else{
+        else {
             this.classList.remove('is-valid');
             this.classList.add('is-invalid');
         }
@@ -57,7 +56,6 @@ function checkName() {
         if (invalidCharacters.test(this.value)) {
             this.value = this.value.replace(invalidCharacters, '');
         }
-        return true;
     });
 }
 
@@ -70,7 +68,6 @@ function checkLastName() {
         if (invalidCharacters.test(this.value)) {
             this.value = this.value.replace(invalidCharacters, '');
         }
-        return true;
     });
 }
 
@@ -80,12 +77,11 @@ function checkEmail() {
 
     emailInput.addEventListener('blur', function (e) { // email vacío
         if (this.value.trim() === '') {
-            
+
         }
         else if (emailRegex.test(this.value.trim())) {  // comprobación email válido
             this.classList.remove('is-invalid');
             this.classList.add('is-valid');
-            return true;
         } else { // email inválido
             this.classList.add('is-invalid');
         }
@@ -104,16 +100,13 @@ function checkPassword() {
         else if (passwordRegex.test(this.value.trim())) {
             this.classList.remove('is-invalid');
             this.classList.add('is-valid');
-            return true;
         } else {
             this.classList.add('is-invalid');
         }
     })
-
-    
 }
 
-function checkConfirmPassword(){
+function checkConfirmPassword() {
     const firstPassword = document.getElementById('input-password');
     const passwordInput = document.getElementById('confirm-password');
     passwordInput.addEventListener('blur', function (e) {
@@ -129,6 +122,32 @@ function checkConfirmPassword(){
     })
 }
 
+/* Funciones para retornar datos booleanos si la validación se efectúa adecuadamente */
+
+function isPhoneValid(value) {
+    return value.length === 12; // que el número tenga formato: 123-456-7890
+}
+
+function isNameValid(value) {
+    return value.trim() !== ''; // Quita espacios en blanco antes y después, checa que no esté vacío (se va a usar para nombre y apellido)
+}
+
+function isEmailValid(value) {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(value.trim()); // compara el email con el regex
+}
+
+function isPasswordValid(value) {
+    const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+    return passwordRegex.test(value.trim()); // compara la contraseña ingresada con el regex, quita espacios antes o después
+}
+
+function isConfirmPasswordValid(password, confirmPassword) {
+    return confirmPassword !== '' && password === confirmPassword; // compara las contraseñas entre sí y que no esté vacía
+}
+
+/* Termina validación con booleanos */
+
 /* Fin validación de datos */
 
 
@@ -140,21 +159,35 @@ const form = document.getElementById('PaginaRegistro');
 form.addEventListener('submit', function (e) {
     e.preventDefault();
 
-    if (checkPhoneNumber() && checkName() && checkLastName() && checkEmail() && checkPassword() && checkConfirmPassword()) {
-        const usuarioJSON = {
-            nombre: form.querySelector('[name="nombre"]').value.trim(),
-            apellido: form.querySelector('[name="apellido"]').value.trim(),
-            telefono: document.getElementById('phone-input').value.trim(),
-            email: form.querySelector('[name="correo"]').value.trim().toLowerCase(),
-            password: form.querySelector('[name="password"]').value
+    const name = form.querySelector('[name="nombre"]').value.trim()
+    const lastName = form.querySelector('[name="apellido"]').value.trim()
+    const phone =  document.getElementById('phone-input').value.trim()
+    const email =  form.querySelector('[name="correo"]').value.trim().toLowerCase()
+    const password = form.querySelector('[name="password"]').value
+    const confirmPassword = form.querySelector('[name="confirmPassword"]').value
+
+    if ( isNameValid(name) && isNameValid(lastName) && isPhoneValid(phone) && isEmailValid(email) && isPasswordValid(password) && isConfirmPasswordValid(password, confirmPassword)) {
+        let usuarioJSON = {
+            nombre: name,
+            apellido: lastName,
+            telefono: phone,
+            email: email,
+            password: password
         };
 
-        const jsonPreview = document.getElementById("jsonPreview");
-        const jsonOutput = document.getElementById("jsonOutput");
+        // const jsonPreview = document.getElementById("jsonPreview");
+        // const jsonOutput = document.getElementById("jsonOutput");
 
-        jsonOutput.textContent = JSON.stringify(usuarioJSON, null, 2);
-        jsonPreview.classList.remove("d-none");
-        jsonPreview.scrollIntoView({ behavior: "smooth", block: "start" });
+        usuarioJSON = JSON.stringify(usuarioJSON, null, 2);
+        console.log(usuarioJSON)
+        localStorage.setItem(email, usuarioJSON)
+        form.reset()
+        // jsonPreview.classList.remove("d-none");
+        // jsonPreview.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+    else {
+        console.log("No se cumplieron las condiciones")
+        console.log(`numero = ${isNameValid(name)}, nombre = ${isNameValid(lastName)}, apellido = ${isPhoneValid(phone)}, email = ${isEmailValid(email)}, contraseña = ${isPasswordValid(password)}, confirmación = ${isConfirmPasswordValid(password, confirmPassword)}`)
     }
     /* Fin creación objetos tipo JSON */
 });
